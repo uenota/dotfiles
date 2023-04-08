399bd7ce57835c7e73c1f2bf514d5e7e17a7ee02
Takaki Ueno <ueknot@gmail.com>
2023-04-08 22:58:30 +0900

    add settings for neovim

--- scripts/link.sh
+++ scripts/link.sh
@@ -6,6 +6,8 @@ for file in .*; do
     ln -s ${PWD}/${file} ${HOME}
 done
 
+ln -s ${dotfiles_root}/dotfiles/.vimrc ${HOME}/.config/nvim/init.vim
+
 # create symlink for fish configs
 ln -s ${dotfiles_root}/fish/config.fish ${HOME}/.config/fish/config.fish
 ln -s ${dotfiles_root}/fish/functions/fish_prompt.fish ${HOME}/.config/fish/functions/fish_prompt.fish
